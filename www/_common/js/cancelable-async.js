fa2a37172cda0c03c4bdf147edcaba1c9906bb4c
Rémi Scandella <remi.scandella@gmail.com>
2020-09-04 03:00:11 +0200

    Don't log

--- www/_common/js/cancelable-async.js
+++ www/_common/js/cancelable-async.js
@@ -1,21 +1,16 @@
-const message = 'Function canceled ; a more recent iteration started';
+const warning = 'Function canceled ; a more recent iteration started';
 
 export function cancelableAsync(generator) {
   let check;
   return async function(...args) {
     const localCheck = check = new Object();
-
     const iterator = generator(...args);
     let lastValue;
     for(;;) {
       const next = iterator.next(lastValue);
-      if (next.done) return next.value; // generator ended
-
+      if (next.done) return next.value;
       lastValue = await next.value;
-      if (localCheck !== check) {
-        console.log(message);
-        return message;
-      }
+      if (localCheck !== check) return warning;
     }
   }
 }
