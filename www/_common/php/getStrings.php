4e45d293a4904b0c6fc438404d20ce896b5546d7
Rémi Scandella <remi.scandella@gmail.com>
2021-01-21 19:45:44 +0100

    Update spacing

--- www/_common/php/getStrings.php
+++ www/_common/php/getStrings.php
@@ -2,25 +2,20 @@
 require_once 'httpLanguage.php';
 
 // Chargement initial des strings dans une application traduisible (via traduction.js)
-class Textes
-{
-    public $strings;
-    public $lang;
+class Textes {
+  public $strings;
+  public $lang;
 
-    function __construct($app)
-    {
-        $this->lang = httpLanguage();
+  function __construct($app) {
+    $this->lang = httpLanguage();
+    $path = dirname(__DIR__, 2) . '/' . $app . '/strings.json';
+    if (!file_exists($path)) throw new Exception('Fichier strings.json introuvable');
+    $json = file_get_contents($path);
+    $json = json_decode($json, true);
+    $this->strings = $json;
+  }
 
-        $path = dirname(__DIR__, 2) . '/' . $app . '/strings.json';
-        if (!file_exists($path))
-            throw new Exception('Fichier strings.json introuvable');
-        $json = file_get_contents($path);
-        $json = json_decode($json, true);
-        $this->strings = $json;
-    }
-
-    public function getString($id)
-    {
-        return $this->strings[$this->lang][$id] ?? $this->strings['fr'][$id] ?? 'undefined string';
-    }
+  public function getString($id) {
+    return $this->strings[$this->lang][$id] ?? $this->strings['fr'][$id] ?? 'undefined string';
+  }
 }
