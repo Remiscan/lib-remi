33d9612d37352152b6dacafb3aea36e65ebb0b4a
Rémi Scandella <remi.scandella@gmail.com>
2021-05-15 18:53:03 +0200

    Add compatibility with more selectors

--- www/_common/components/theme-selector/build-css.php
+++ www/_common/components/theme-selector/build-css.php
@@ -48,23 +48,25 @@
 
 function buildThemesStylesheet($body, $default = 'light')
 {
-  $regexp = '/(?:html|:root)\[data-theme="?light"?\] *\{(?<light>(?:.|\n|\r)*)\}(?:\n|\r| )*(?:html|:root)\[data-theme="?dark"?\] *\{(?<dark>(?:.|\n|\r)*)\}/';
+  $regexp = '/(?:html|:root)\[data-theme="?light"?\](?<selector> .*)*\{(?<light>(?:.|\n|\r)*)\}(?:\n|\r| )*(?:html|:root)\[data-theme="?dark"?\]( .*)*\{(?<dark>(?:.|\n|\r)*)\}/';
   preg_match_all($regexp, $body, $matches);
 
+  $selector = str_replace(' ', '', $matches['selector'][0]) == '' ? ':root' : $matches['selector'][0];
+
   $css = "*/
-:root {" . $matches[$default][0] . "}
+" . $selector . " {" . $matches[$default][0] . "}
 
 @media (prefers-color-scheme: light) {
-  :root {" . $matches['light'][0] . "}
+  " . $selector . " {" . $matches['light'][0] . "}
 }
 
 @media (prefers-color-scheme: dark) {
-  :root {" . $matches['dark'][0] . "}
+  " . $selector . " {" . $matches['dark'][0] . "}
 }
 
-:root[data-theme=\"light\"] {" . $matches['light'][0] . "}
+:root[data-theme=\"light\"] " . $matches['selector'][0] . " {" . $matches['light'][0] . "}
 
-:root[data-theme=\"dark\"] {" . $matches['dark'][0] . "}
+:root[data-theme=\"dark\"] " . $matches['selector'][0] . " {" . $matches['dark'][0] . "}
 /*";
 
   return $css;
