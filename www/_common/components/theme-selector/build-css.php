bfd844154bad8c25fbcf73b4be6251cb4d8febec
Rémi Scandella <remi.scandella@gmail.com>
2021-05-23 18:35:22 +0200

    Don't add (prefers-color-scheme: {default})

--- www/_common/components/theme-selector/build-css.php
+++ www/_common/components/theme-selector/build-css.php
@@ -46,28 +46,23 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function buildThemesStylesheet($body, $default = 'light')
+function buildThemesStylesheet($body, $default = 'light', $closeComment = true)
 {
   $regexp = '/(?:html|:root)\[data-theme="?light"?\](?<selector> .*)*\{(?<light>(?:.|\n|\r)*?)\}(?:\n|\r| )*(?:html|:root)\[data-theme="?dark"?\]( .*)*\{(?<dark>(?:.|\n|\r)*?)\}/';
   preg_match_all($regexp, $body, $matches);
 
   $selector = str_replace(' ', '', $matches['selector'][0]) == '' ? ':root' : $matches['selector'][0];
 
-  $css = "*/
-" . $selector . " {" . $matches[$default][0] . "}
-
-@media (prefers-color-scheme: light) {
-  " . $selector . " {" . $matches['light'][0] . "}
-}
-
-@media (prefers-color-scheme: dark) {
-  " . $selector . " {" . $matches['dark'][0] . "}
-}
-
-:root[data-theme=\"light\"] " . $matches['selector'][0] . " {" . $matches['light'][0] . "}
-
-:root[data-theme=\"dark\"] " . $matches['selector'][0] . " {" . $matches['dark'][0] . "}
-/*";
+  $css = "";
+  if ($closeComment) $css .= "*/";
+  $css .= "\n" . $selector . " {" . $matches[$default][0] . "}\n\n";
+  if ($default == 'dark')
+    $css .= "@media (prefers-color-scheme: light) {\n" . $selector . " {" . $matches['light'][0] . "}\n}\n\n";
+  else
+    $css .= "@media (prefers-color-scheme: dark) {\n" . $selector . " {" . $matches['dark'][0] . "}\n}\n\n";
+  $css .= ":root[data-theme=\"light\"] " . $matches['selector'][0] . " {" . $matches['light'][0] . "}\n\n";
+  $css .= ":root[data-theme=\"dark\"] " . $matches['selector'][0] . " {" . $matches['dark'][0] . "}\n";
+  if ($closeComment) $css .= "/*";
 
   return $css;
 }
