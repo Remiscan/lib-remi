76fe27b1cbe7e3ec04e2043097dcf46943c8ebbd
Rémi Scandella <remi.scandella@gmail.com>
2021-06-11 14:21:22 +0200

    Add sample custom-elements

---
+++ www/_common/components/__samples/no-shadow-dom/sample-element.js.php
@@ -0,0 +1,29 @@
+let cssReady = false;
+const css = `<?php include './style.css.php'; ?>`;
+const html = `<?php include './element.html'; ?>`;
+
+
+
+class SampleElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+
+  connectedCallback() {
+    // Add CSS to the page
+    if (!cssReady) {
+      const head = document.querySelector('head');
+      const firstStylesheet = document.querySelector('link[rel="stylesheet"], style');
+      const style = document.createElement('style');
+      style.innerHTML = css;
+      style.id = 'sample-element-style';
+      if (!!firstStylesheet)  head.insertBefore(style, firstStylesheet);
+      else                    head.appendChild(style);
+      cssReady = true;
+    }
+    this.innerHTML = html;
+  }
+}
+
+if (!customElements.get('sample-element')) customElements.define('sample-element', SampleElement);
---
+++ www/_common/components/__samples/no-shadow-dom/style.css.php
@@ -0,0 +1,5 @@
+sample-element {
+  --rules: 'here';
+}
+
+/* Every rule must be in a sample-element>whatever selector */
