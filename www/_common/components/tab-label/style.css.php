c3627c6b649b349cf3eb7cd5de1d5865a202721a
Rémi Scandella <remi.scandella@gmail.com>
2021-06-11 21:02:00 +0200

    Update default styles

--- www/_common/components/tab-label/style.css.php
+++ www/_common/components/tab-label/style.css.php
@@ -2,7 +2,7 @@
 :root[data-theme="light"] tab-label, [role="tablist"] {
   --on-bg-color: hsl(231, 40%, 50%);
   --hover-bg-color: hsl(231, 40%, 50%, .3);
-  --active-bg-color: hsl(231, 40%, 50%, .6);
+  --active-bg-color: hsl(231, 40%, 50%, .4);
   --off-text-color: black;
   --on-text-color: white;
 }
@@ -10,7 +10,7 @@
 :root[data-theme="dark"] tab-label, [role="tablist"] {
   --on-bg-color: hsl(217, 89%, 75%);
   --hover-bg-color: hsl(217, 89%, 75%, .3);
-  --active-bg-color: hsl(217, 89%, 75%, .6);
+  --active-bg-color: hsl(217, 89%, 75%, .4);
   --off-text-color: white;
   --on-text-color: rgb(48, 48, 48);
 }
@@ -18,24 +18,24 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/components/theme-selector/build-css.php';
 echo buildThemesStylesheet($body); ?>*/
 
-tab-label {
-  display: grid;
-  place-items: center;
-}
-
 [role="tablist"] {
   display: flex;
   margin: 0;
   padding: 0;
   border: none;
-  gap: .6rem;
-  padding: 0 .6rem;
+  gap: .6em;
+  padding: 0 .6em;
   box-shadow: inset 0 -1px 0 var(--on-bg-color);
 }
 
 [role="tablist"]>legend {
   font-size: 0;
   padding: 0;
+}
+
+tab-label {
+  display: grid;
+  place-items: center;
 }
 
 input[type=radio][role="tab"] {
@@ -48,7 +48,7 @@ input[type=radio][role="tab"] {
 }
 
 input[type=radio][role="tab"] + label {
-  padding: .3rem .6rem;
+  padding: .3em .6em;
   border: 1px solid var(--on-bg-color);
   color: var(--off-text-color);
 }
