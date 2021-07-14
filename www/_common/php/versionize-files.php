4acce578a6ba54e522c5ba9a040603f1a234c70b
Rémi Scandella <remi.scandella@gmail.com>
2021-07-15 00:52:03 +0200

    Make compatible with CSS links, and move examples

--- www/_common/php/versionize-files.php
+++ www/_common/php/versionize-files.php
@@ -1,61 +1,21 @@
 <?php
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Récupère les imports JavaScript en utilisant la fonction comme ceci en début de fichier .js.php :
-//
-//    /*< ?php ob_start();? >*/
-//
-//    import { ... } from '../_common/abc.js';
-//    import { ... } from './abc.js.php';
-//    import { ... } from '/abc.js.php';
-//    import { ... } from 'abc.js.php';
-//    import './abc.js.php';
-//
-//    /*< ?php $body = ob_get_clean();
-//    require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
-//    echo versionizeFiles($body, __DIR__); ? >*/
-//
-// et renvoie les imports en ayant inséré le numéro de version avant l'extension de fichier :
-//
-//    import { ... } from '../_common/abc--2020.01.30_16.16.16.js';
-//    import { ... } from './abc--2020.03.11_20.31.18.js.php';
-//    import './abc--2020.03.11_20.31.18.js.php';
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Récupère aussi les liens de fichiers dans les attributs src ou href comme ceci :
-//
-//    <!--< ?php ob_start();? >-->
-//
-//    <link rel="preload" as="fetch" href="/app/strings.json" crossorigin>
-//    <link rel="modulepreload" href="/_common/js/traduction.js">
-//    <link rel="stylesheet" href="/app/page.css">
-//    <script type="module" src="/app/scripts.js.php"></script>
-//
-//    <!--< ?php $imports = ob_get_clean();
-//    require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
-//    echo versionizeFiles($imports, __DIR__); ? >-->
-//
-// et renvoie les liens en ayant inséré le numéro de version avant l'extension de fichier :
-//
-//    <link rel="preload" as="fetch" href="/app/strings--2020.08.26_23.45.06.json" crossorigin>
-//    <link rel="modulepreload" href="/_common/js/traduction--2020.08.27_23.22.24.js">
-//    <link rel="stylesheet" href="/app/page--2020.08.25_22.51.29.css">
-//    <script type="module" src="/app/scripts--2020.08.28_22.25.39.js.php"></script>
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 require_once __DIR__ . '/version.php';
 require_once __DIR__ . '/resolvePath.php';
 require_once __DIR__ . '/ModuleList.php';
 
-function versionizeFiles($body, $fromDir = __DIR__)
-{
-  $extensions = ['\.html', '\.css', '\.json', '\.js', '(?:\.js|\.css)\.php'];
+
+
+/**
+ * Captures a body of JavaScript, CSS or HTML, and versionizes links in that body.
+ * (See below for examples.)
+ */
+function versionizeFiles(string $body, string $fromDir = __DIR__): string {
+  $extensions = 'css|css\.php|js|js\.php|json|htm|svg|ico|webp|png|jpe?g|gif|mp4'; // same as in .htaccess
   $regexps = [
-    '/(?:from|import) +?\'((?:.*?\/)([^\/]+?)(\.js(?:\.php)?))\';/',
-    '/(?:src|href) *?= *?"((?:[^"]*?\/)?([^\/<>]+?)(' . implode('|', $extensions) . '))"/',
-    '/(?:new Worker\()\'((?:.*?\/)([^\/]+?)(\.js(?:\.php)?))\'/'
+    '/(?:from|import) +?\'((?:.*?\/)([^\/]+?)\.(js(?:\.php)?))\';/',
+    '/(?:new Worker\()\'((?:.*?\/)([^\/]+?)\.(js(?:\.php)?))\'/',
+    '/(?:src|href) *?= *?"((?:[^"]*?\/)?([^\/<>]+?)\.('.$extensions.'))"/',
+    '/(?:url\()\'((?:.*?\/)([^\/]+?)\.('.$extensions.'))\'/'
   ];
   $allMatches = ['full' => [], 'path' => [], 'filename' => [], 'fileext' => []];
 
@@ -70,8 +30,7 @@ function versionizeFiles($body, $fromDir = __DIR__)
   }
 
   $n = count($allMatches['full']);
-  for ($i = 0; $i < $n; $i++)
-  {
+  for ($i = 0; $i < $n; $i++) {
     $match = array(
       'full' => $allMatches['full'][$i],
       'path' => $allMatches['path'][$i],
@@ -91,10 +50,59 @@ function versionizeFiles($body, $fromDir = __DIR__)
 
     $body = str_replace(
       $match['path'],
-      $Path->resolve(false, 'absolute') . '/' . $match['filename'] . '--' . $version . $match['fileext'],
+      $Path->resolve(false, 'absolute') . '/' . $match['filename'] . '--' . $version . '.' . $match['fileext'],
       $body
     );
   }
 
   return $body;
 }
+
+
+
+// Example 1: versionize links in a JavaScript (.js.php) file:
+//
+//    /*< ?php ob_start();? >*/
+//
+//    import { ... } from '../_common/abc.js';
+//    import { ... } from './abc.js.php';
+//    import { ... } from '/abc.js.php';
+//    import { ... } from 'abc.js.php';
+//    import './abc.js.php';
+//    new Worker('worker-link.js');
+//
+//    /*< ?php $body = ob_get_clean();
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
+//    echo versionizeFiles($body, __DIR__); ? >*/
+//
+//        returns:
+//
+//    import { ... } from '/_common/abc--1564499867.js';
+//    ...
+//    import './abc--1564499867.js.php';
+//    new Worker('worker-link--1564499867.js');
+
+
+// Example 2: versionize links in a HTML (.php) file:
+//
+//    <!--< ?php ob_start();? >-->
+//
+//    <link rel="stylesheet" href="/app/page.css">
+//    <script type="module" src="/app/scripts.js.php"></script>
+//
+//    <!--< ?php $body = ob_get_clean();
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
+//    echo versionizeFiles($body, __DIR__); ? >-->
+
+
+// Example 3: versionize links in a CSS (.css.php) file:
+//
+//    <!--< ?php ob_start();? >-->
+//
+//    :root {
+//      background-image: url('./image.webp');
+//    }
+//
+//    <!--< ?php $body = ob_get_clean();
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
+//    echo versionizeFiles($body, __DIR__); ? >-->
