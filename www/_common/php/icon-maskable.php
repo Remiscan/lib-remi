50524351673c6e3db4139596b8c21582d26abd6d
Rémi Scandella <remi.scandella@gmail.com>
2021-12-06 13:22:34 +0100

    New maskable icon system

---
+++ www/_common/php/icon-maskable.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: image/svg+xml');
+$url = (isset($_GET['icon'])) ? dirname(__DIR__, 2).'/'.$_GET['icon'] : __DIR__.'/icon.svg';
+
+ob_start();
+include $url;
+$svg = ob_get_clean();
+
+$svg = str_replace('viewBox="51.2 51.2 409.6 409.6"', 'viewBox="0 0 512 512"', $svg);
+echo $svg;
