e34c3875ee0da20b798309b0ee7797e1fd494caa
Rémi Scandella <remi.scandella@gmail.com>
2021-12-25 15:40:12 +0100

    Update sample custom elements with CSS modules

---
+++ www/_common/components/_samples/no-shadow-dom/sample-element.js
@@ -0,0 +1,34 @@
+import sheet from './styles.css' assert { type: 'css' };
+import template from './template.js';
+
+
+
+class SampleElement extends HTMLElement {
+  constructor() {
+    super();
+    this.ready = false;
+  }
+
+
+  update(attr, oldValue, newValue) {
+    if (!this.ready) return;
+  }
+
+
+  connectedCallback() {
+    // Add HTML and CSS to the element
+    if (!document.adoptedStyleSheets.includes(sheet))
+      document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+    this.appendChild(template.content.cloneNode(true));
+
+    // Do other stuff here
+  }
+
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this.update(name, oldValue, newValue);
+  }
+}
+
+if (!customElements.get(sample-element)) customElements.define(sample-element, SampleElement);
