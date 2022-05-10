3702afbf94334245f220c2574f112f5fb4f61c6b
Rémi Scandella <remi.scandella@gmail.com>
2022-05-10 14:46:24 +0200

    Support native focus outline

--- www/_common/components/input-switch/template.js
+++ www/_common/components/input-switch/template.js
@@ -1,13 +1,15 @@
 const template = document.createElement('template');
 template.innerHTML = `
-<button type="button" role="switch" aria-checked="false">
-  <span class="input-switch-hints" aria-hidden="true">
-    <span data-state="on"></span>
-    <svg class="input-switch-handle" viewBox="0 0 24 24">
-      <circle cx="12" cy="12" r="9.6" stroke-width="0"/>
-      <circle class="focus-dot" cx="12" cy="12" r="2.4" stroke-width="0"/>
-    </svg>
-    <span data-state="off"></span>
+<button type="button" role="switch" aria-checked="false" part="button">
+  <span class="input-switch-bg" aria-hidden="true">
+    <span class="input-switch-hints" aria-hidden="true" part="hints">
+      <span data-state="on" part="hint-on"></span>
+      <svg class="input-switch-handle" viewBox="0 0 24 24" part="handle">
+        <circle cx="12" cy="12" r="9.6" stroke-width="0"/>
+        <circle class="focus-dot" cx="12" cy="12" r="2.4" stroke-width="0"/>
+      </svg>
+      <span data-state="off" part="hint-off"></span>
+    </span>
   </span>
 </button>
 `;
