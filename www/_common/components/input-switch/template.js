68ed4eb00f92537af4edb84be55a3371f5fdbda2
Rémi Scandella <remi.scandella@gmail.com>
2022-05-10 14:47:27 +0200

    Update part names

--- www/_common/components/input-switch/template.js
+++ www/_common/components/input-switch/template.js
@@ -2,13 +2,13 @@ const template = document.createElement('template');
 template.innerHTML = `
 <button type="button" role="switch" aria-checked="false" part="button">
   <span class="input-switch-bg" aria-hidden="true">
-    <span class="input-switch-hints" aria-hidden="true" part="hints">
-      <span data-state="on" part="hint-on"></span>
+    <span class="input-switch-hints" aria-hidden="true" part="hints-container">
+      <span data-state="on" part="hint hint-on"></span>
       <svg class="input-switch-handle" viewBox="0 0 24 24" part="handle">
         <circle cx="12" cy="12" r="9.6" stroke-width="0"/>
         <circle class="focus-dot" cx="12" cy="12" r="2.4" stroke-width="0"/>
       </svg>
-      <span data-state="off" part="hint-off"></span>
+      <span data-state="off" part="hint hint-off"></span>
     </span>
   </span>
 </button>
