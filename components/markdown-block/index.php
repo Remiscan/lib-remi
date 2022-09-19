d04408d2f7ef70af034af7bfa1b63b05649ef806
Rémi Scandella <remi.scandella@gmail.com>
2022-09-19 23:22:26 +0200

    Enable a markdown-block to parse another element

--- components/markdown-block/index.php
+++ components/markdown-block/index.php
@@ -42,13 +42,17 @@
     }
   }
 
+  .intro { 
+    opacity: .8;
+  }
+
   pre {
     white-space: pre-wrap;
   }
 
   markdown-block:not([parsed]),
   markdown-block:not(:defined) > * {
-    display: none;
+    opacity: 0;
   } 
 </style>
 
@@ -58,8 +62,25 @@
 
 <body>
 
-<pre>
-# Example
+<h2 class="intro">This column is Markdown.</h2>
+<h2 class="intro">This column is HTML.</h2>
+
+<pre id="inert" contenteditable="true">
+# First example
+
+Hello, I am an example.
+</pre>
+
+<markdown-block>
+# First example
+
+Hello, I am an example.
+</markdown-block>
+
+<pre id="test" contenteditable="true">
+# Editable example
+
+## You can edit this example on the left side, and the markdown-block on the right will automatically be updated.
 
 A third of the distance across the Beach, the meadow ends and sand begins. This slopes gradually up for another third of the distance, to the foot of the sand hills, which seem tumbled into their places by some mighty power, sometimes three tiers of them deep, sometimes two, _and sometimes only one._ A third of the distance across the Beach, the meadow ends and sand begins.
 
@@ -159,104 +180,6 @@ function codeBlocks() {
 </pre>
 
 
-<markdown-block>
-# Example
-
-A third of the distance across the Beach, the meadow ends and sand begins. This slopes gradually up for another third of the distance, to the foot of the sand hills, which seem tumbled into their places by some mighty power, sometimes three tiers of them deep, sometimes two, _and sometimes only one._ A third of the distance across the Beach, the meadow ends and sand begins.
-
-The outline of this inner shore is most irregular, curving and bending in and out and back upon itself, making coves and points and creeks and channels, and often pushing out in flats with not water enough on them at low tide to wet your ankles.
-
-## Subtitle
-
-This is another fine paragraph
-
-### Smaller subtitle
-
-This is a paragraph `with` ~style~ *italic* _italic_ **bold** __bold__
-
-![image](https://remiscan.fr/mon-portfolio/images/moi.jpg)
-
-*Hello [link](https://remiscan.fr) lol*
-
-Hello [*link*](https://remiscan.fr) lol "cat"
-
-Hello from *[link](https://remiscan.fr)* to __everyone__ `reading this`
-
-Here's an [**important** anchor link](#example).
-
-line 1
-line 2
-
-Code & Poetry
--------------
-
-    You can also indent
-    blocks to display
-    code or poetry.
-
-    Indented code/poetry blocks
-    can be hard-wrapped.
-
-**Or, wrap your code in three backticks:**
-
-```js
-function codeBlocks() {
-  return "Can be inserted"
-}
-```
-
-
-### Block Quotes
-
-> You can insert quotes by
-> preceeding each line with `>`.
->
-> Blockquotes can also contain line
-> breaks.
-
-
-## Lists
-
-### Unordered lists
-
-- Unordered
-- Lists
-- Hello
-
-### Ordered lists
-
-1. Ordered
-2. Lists
-4. Numbers are ignored
-1. Ordered
-
-121) Ordered lists can start
-122) with any number and
-123) use . as well as ) as a separator.
-
-### Task lists
-
-- [ ] Task 1
-- [x] Task 2
-- [ ] Task 3
-- Regular list item
-
-## Tables
-
-| Column 1 | Column 2 | Column 3 | Column 4
-|----------|:---------|:--------:|---------:
-| default | left | center | right
-
-### Table of image file types
-
-| Header                    | Mime type    | Extensions | Description
-|---------------------------|--------------|------------|-------------
-| `89 50 4E 47 0D 0A 1A 0A` | image/png    | png        | PNG image
-| `47 49 46 38 39 61`       | image/gif    | gif        | GIF image
-| `FF D8 FF`                | image/jpeg   | jpg jpeg   | JPEG image
-| `4D 4D 00 2B`             | image/tiff   | tif tiff   | TIFF image
-| `42 4D`                   | image/bmp    | bmp        | Bitmap image
-| `00 00 01 00`             | image/x-icon | ico        | Icon image
-</markdown-block>
+<markdown-block source="test"></markdown-block>
 
 </body>
--- components/markdown-block/markdown-block.js
+++ components/markdown-block/markdown-block.js
@@ -9,35 +9,97 @@
 </script>
 */
 
-import * as markdown from 'markdown-wasm';
+import * as Parser from 'markdown-wasm';
 
 
 
 export class MarkdownBlock extends HTMLElement {
   constructor() {
     super();
-    this.parsed = false;
+    
+    this.parsed = false; // Whether the source's content is already parsed.
+    this.parsing = false; // Whether the source's content is currently being parsed.
+    this.source = null; // ID of the source element whose content will be parsed.
+
+    // MutationObserver that will observe text changes inside the source element.
+    this.observer = new MutationObserver(mutationList => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'characterData') {
+          if (this.parsing) return;
+
+          // Reset the block's state
+          this.parsed = false;
+          this.removeAttribute('parsed');
+
+          this.parseContent();
+        }
+      }
+    });
+  }
+
+
+  /** Gets the content of the source element and converts it from Markdown to HTML. */
+  async parseContent() {
+    if (this.parsed || this.parsing) return;
+    this.parsing = true;
+
+    // Get the content of the source element
+    const sourceID = this.getAttribute('source');
+    const source = document.getElementById(sourceID) || this;
+    if (this.source !== source) {
+      this.source = source;
+      if (this.source !== this) this.observeSource();
+    }
+
+    // Parse the source's content and inset it into the block
+    const markdown = this.source.innerHTML;
+    await Parser.ready;
+    const html = Parser.parse(markdown);
+    this.innerHTML = html;
+    console.log('content parsed', this.source, Date.now());
+
+    // Mark the block as parsed
+    this.setAttribute('parsed', '');
+    this.parsed = true;
     this.parsing = false;
   }
 
 
-  async parseContent() {
-    if (this.parsing) return;
-    this.parsing = true;
-
-    const markdownContent = this.innerHTML;
-    await markdown.ready;
-    const HTMLContent = markdown.parse(markdownContent);
-    this.innerHTML = HTMLContent;
-
-    this.setAttribute('parsed', '');
-    this.parsed = true;
+  /** Changes the source element. */
+  observeSource() {
+    this.observer.observe(this.source, { characterData: true, subtree: true });
+    console.log('observed:', this.source);
   }
 
 
   connectedCallback() {
-    if (this.parsed || this.parsing) return;
-    this.parseContent();
+    // If a source element is already defined, observe it.
+    if (this.source && this.source !== this) this.observeSource();
+
+    // If no source element is defined, markdown-block is its own source element. Parse its content.
+    else this.parseContent();
+  }
+
+  disconnectedCallback() {
+    this.observer.disconnect();
+  }
+
+
+  static get observedAttributes() {
+    return ['source'];
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    switch (attr) {
+      case 'source': {
+        // Reset the block's state
+        this.parsed = false;
+        this.removeAttribute('parsed');
+
+        this.parseContent();
+      } break;
+    }
   }
 }
 
