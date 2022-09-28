1922c2c12723dc4c7ecca8424565108c0f7067f8
Rémi Scandella <remi.scandella@gmail.com>
2022-09-28 15:04:22 +0200

    Use clearer point transform functions

--- components/artsy-block/worklets/diamond-cells.js
+++ components/artsy-block/worklets/diamond-cells.js
@@ -7,6 +7,35 @@ function mulberry32(a) {
   }
 }
 
+class Point2D {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  translate(dx = 0, dy = 0) {
+    return new Point2D(
+      this.x + dx,
+      this.y + dy
+    );
+  }
+
+  rotate(angle = 0) {
+    const rad = angle * Math.PI / 180;
+    return new Point2D(
+      Math.cos(rad) * this.x - Math.sin(rad) * this.y,
+      Math.sin(rad) * this.x + Math.cos(rad) * this.y
+    );
+  }
+
+  scale(sx = 1, sy = sx) {
+    return new Point2D(
+      sx * this.x,
+      sy * this.y
+    );
+  }
+}
+
 
 
 registerPaint('diamond-cells', class {
@@ -22,48 +51,53 @@ registerPaint('diamond-cells', class {
     const columns = Math.ceil(size.width / cellSize);
     const rows = Math.ceil(size.height / cellSize);
 
-    for (let row = -1; row < rows + 1; row++) {
-      for (let col = -1; col < columns + 1; col++) {
-        const random = mulberry32(Number(props.get('--base-seed')) * (row + 2) * (col + 2));
+    // Base shape
+    const origin = new Point2D(0, 0);
+    const corners = [
+      origin.translate(0, 0),
+      origin.translate(cellSize, 0),
+      origin.translate(cellSize, cellSize),
+      origin.translate(0, cellSize)
+    ];
 
-        const rand = Math.round(random() * 100);
+    for (let row = -1; row <= rows + 1; row++) {
+      for (let col = -1; col <= columns + 1; col++) {
+        const random = mulberry32(baseSeed * (row + 2) * (col + 2));
+
+        // Only display frequency% of cells
+        const rand = Math.ceil(random() * 100);
         if (rand > frequency) continue;
 
-        // Generate and paint individual cells here
         const offset = {
           x: 0.5 * (-1 + random() * 2) * cellSize,
           y: 0.5 * (-1 + random() * 2) * cellSize,
         }
 
-        const scale = Math.round(100 * (1 - .9 * random())) / 100;
-        const angle = 45 * Math.PI / 180;
+        const scale = Math.round(100 * (.6 - .5 * random())) / 100;
+        const angle = 45;
 
-        const position = {
-          x: col * cellSize + offset.x,
-          y: row * cellSize + offset.y
-        };
+        const placedCorners = corners.map(point => point
+          .translate(-5 * cellSize, -5 * cellSize)    // move origin to center of shape
+          .scale(scale)                               // scale shape
+          .rotate(angle)                              // rotate shape
+          .translate(.5 * cellSize, .5 * cellSize)    // move origin back to top left of shape
+          .translate(offset.x, offset.y)              // move shape by random offset
+          .translate(col * cellSize, row * cellSize)  // move shape to correct row and column
+        );
 
-        const scaledPosition = {
-          x: position.x + 0.5 * (1 - scale) * cellSize,
-          y: position.y + 0.5 * (1 - scale) * cellSize
-        };
-
-        const rotatedPosition = {
-          x: Math.cos(-angle) * scaledPosition.x + -Math.sin(-angle) * scaledPosition.y,
-          y: Math.sin(-angle) * scaledPosition.x + Math.cos(-angle) * scaledPosition.y
-        };
-
+        // Randomize cell color
         const hue = Number(props.get('--base-hue')) + (-1 + 2 * random()) * Number(props.get('--max-hue-spread'));
-        const opacity = Math.round(100 * random()) / 100;
+        const opacity = Math.round(100 * (1 - 1 * random())) / 100;
         ctx.fillStyle = `hsl(${hue}, 100%, 50%, ${opacity})`;
 
-        ctx.save();
-        
-        ctx.translate((position.x + cellSize) / 2, (position.y + cellSize) / 2);
-        ctx.rotate(angle);
-        ctx.fillRect(rotatedPosition.x, rotatedPosition.y, cellSize * scale, cellSize * scale);
-
-        ctx.restore();
+        // Draw the cell
+        const shape = new Path2D();
+        shape.moveTo(placedCorners[0].x, placedCorners[0].y);
+        for (const corner of placedCorners) {
+          shape.lineTo(corner.x, corner.y);
+        }
+        shape.closePath();
+        ctx.fill(shape);
       }
     }
   }
