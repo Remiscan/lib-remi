1935798ab3d1b656e2913f6f1fee4225596afdfb
Rémi Scandella <remi.scandella@gmail.com>
2022-09-28 15:55:08 +0200

    Add Point2D class for 2D point transforms

---
+++ js/geometry.js
@@ -0,0 +1,28 @@
+export class Point2D {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  translate(dx = 0, dy = 0) {
+    return new Point2D(
+      this.x + dx,
+      this.y + dy
+    );
+  }
+
+  rotate(angle = 0) {
+    const rad = angle * Math.PI / 180;
+    return new Point2D(
+      Math.cos(rad) * this.x - Math.sin(rad) * this.y,
+      Math.sin(rad) * this.x + Math.cos(rad) * this.y
+    );
+  }
+
+  scale(sx = 1, sy = sx) {
+    return new Point2D(
+      sx * this.x,
+      sy * this.y
+    );
+  }
+}
