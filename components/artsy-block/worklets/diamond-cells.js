291cfbc44bf82fffe41c2c3570ee32c1183d200c
Rémi Scandella <remi.scandella@gmail.com>
2022-09-28 17:09:57 +0200

    Make saturation and lightness variables too

--- components/artsy-block/worklets/diamond-cells.js
+++ components/artsy-block/worklets/diamond-cells.js
@@ -5,7 +5,7 @@ import { mulberry32, xmur3a } from '/_common/js/prng.js';
 
 registerPaint('diamond-cells', class {
   static get contextOptions() { return {alpha: true}; }
-  static get inputProperties() { return ['--base-seed', '--cell-size', '--frequency', '--base-hue', '--max-hue-spread', '--angle-coeff']; }
+  static get inputProperties() { return ['--base-seed', '--cell-size', '--frequency', '--base-hue', '--base-saturation', '--base-lightness', '--max-hue-spread', '--angle-coeff']; }
 
   paint(ctx, size, props) {
     const baseSeed = props.get('--base-seed');
@@ -13,6 +13,10 @@ registerPaint('diamond-cells', class {
     const cellSize = Math.max(3, Number(props.get('--cell-size'))) || 40;
     const frequency = Number(props.get('--frequency')) ?? 100;
     const angleCoeff = Number(props.get('--angle-coeff'));
+    const baseHue = Number(props.get('--base-hue'));
+    const baseSaturation = Number(props.get('--base-saturation'));
+    const baseLightness = Number(props.get('--base-lightness'));
+    const maxHueSpread = Number(props.get('--max-hue-spread'));
 
     const columns = Math.ceil(size.width / cellSize);
     const rows = Math.ceil(size.height / cellSize);
@@ -53,9 +57,9 @@ registerPaint('diamond-cells', class {
         );
 
         // Randomize cell color
-        const hue = Number(props.get('--base-hue')) + (-1 + 2 * random()) * Number(props.get('--max-hue-spread'));
+        const hue = baseHue + (-1 + 2 * random()) * maxHueSpread;
         const opacity = Math.round(100 * (1 - 1 * random())) / 100;
-        ctx.fillStyle = `hsl(${hue}, 100%, 50%, ${opacity})`;
+        ctx.fillStyle = `hsl(${hue}, ${baseSaturation}%, ${baseLightness}%, ${opacity})`;
 
         // Draw the cell
         const shape = new Path2D();
