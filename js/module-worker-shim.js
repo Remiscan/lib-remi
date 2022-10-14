918c0702a367618f36e46dbf8c55a41c86f661e6
Rémi Scandella <remi.scandella@gmail.com>
2022-10-14 13:01:39 +0200

    Extend Worker class

--- js/module-worker-shim.js
+++ js/module-worker-shim.js
@@ -1,13 +1,13 @@
 // For use with es-module-shims.js in Shim mode
 // i.e. with window.esmsInitOptions.shimMode = true
 
-export default class ModuleWorkerShim {
+export default class ModuleWorkerShim extends Worker {
   /**
    * Takes an ES module and turns it into a worker that supports import maps.
    * @param {string} url - The full URL of the ES module to execute in the worker.
    */
   constructor(url) {
-    return new Worker(
+    super(
       URL.createObjectURL(
         new Blob(
           [`
