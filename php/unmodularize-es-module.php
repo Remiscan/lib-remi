93497b72135bb111ab90d80020682acae2ba79e1
Rémi Scandella <remi.scandella@gmail.com>
2022-10-14 12:59:43 +0200

    Add comments

--- php/unmodularize-es-module.php
+++ php/unmodularize-es-module.php
@@ -1,19 +1,31 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/_common/php/ModuleGraph.php';
 
+/**
+ * Builds a module graph starting from a first ES module, then include all of these modules
+ * in the same file in proper import order, and remove all mentions of import and export.
+ * This effectively transforms multiple ES module files into a single, non-module JS file.
+ * @param $moduleId - The identifier of the first module used in the import map.
+ * @param $importMapPath - The path of the import map file.
+ */
 function unmodularize(string $moduleId, string $importMapPath) {
+  // Build the module graph
   $importMap = json_decode(file_get_contents($importMapPath), associative: true)['imports'];
   $graph = ModuleGraph::create($moduleId, $importMap);
   $orderedModules = array_reverse(
     $graph->topologicalOrder()
   );
   
+  // Include all module files in the right order and remove imports/exports.
   foreach ($orderedModules as $module) {
     $path = $module->id;
+
     ob_start();
     include_once $path;
     $content = ob_get_clean();
+
     echo preg_replace_callback_array([
+      // Replace 'import/export { name as alias };' by 'var alias = name;'
       '/(?:export|import) *?\{(.*?)\} *?(?:from(.*?))?;/' => function ($matches) {
         $body = $matches[1];
         $parts = explode(',', $body);
@@ -32,9 +44,12 @@ function unmodularize(string $moduleId, string $importMapPath) {
 
         return $result;
       },
-      '/export(.+?);/' => function ($matches) { return ''; },
+
+      // Replace 'import/export name;' by ''
+      '/(?:export|import)(.+?);/' => function ($matches) { return ''; },
+      
+      // Replace 'export expr' by 'expr';
       '/export ?/' => function ($matches) { return ''; },
-      '/import(.+?);/' => function ($matches) { return ''; },
     ], $content);
   }
 }
