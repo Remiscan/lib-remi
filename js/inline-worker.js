72b6870f4d1dea4db5d8c689ea49ce81de869f17
Rémi Scandella <remi.scandella@gmail.com>
2022-10-20 02:43:31 +0200

    Extend Worker class

--- js/inline-worker.js
+++ js/inline-worker.js
@@ -1,4 +1,7 @@
-/* HOW TO USE :
+/* 
+
+  HOW TO USE :
+  ------------
 
   import InlineWorker from 'inline-worker';
 
@@ -10,7 +13,6 @@
   const inlineWorker = new InlineWorker(f);
 
   // For example, run the function in the worker on a button click
-  const button = document.querySelector('button');
   button.addEventListener('click', async event => {
     const result = await inlineWorker.run(...args);
     console.log(result);
@@ -18,23 +20,26 @@
 
 */
 
-export default class InlineWorker {
+export default class InlineWorker extends Worker {
   /**
    * Makes a Worker by calling new InlineWorker().
    * @param {function} asyncFunc - An async function that will be ran in the worker.
    */
   constructor(asyncFunc, options) {
-    const workerScript = `f=${asyncFunc}; onmessage=${async e => {
-      let result;
-      try {
-        result = await f.apply(f, JSON.parse(e.data));
-        postMessage(JSON.stringify(result));
-      } catch (error) {
-        console.error(error);
-      }
-    }};`;
-    const workerUrl = URL.createObjectURL(new Blob([workerScript]));
-    this.worker = new Worker(workerUrl);
+    const workerScript = `
+      f=${asyncFunc};
+      onmessage=${async e => {
+        let result;
+        try {
+          result = await f.apply(f, JSON.parse(e.data));
+          postMessage(JSON.stringify(result));
+        } catch (error) {
+          console.error(error);
+        }
+      }};
+    `;
+    const workerUrl = URL.createObjectURL(new Blob([workerScript], { type: 'application/javascript' }));
+    super(workerUrl, options);
   }
 
   /**
@@ -45,22 +50,22 @@ export default class InlineWorker {
   async run(...args) {
     let handler;
     return await new Promise((resolve, reject) => {
-      this.worker.addEventListener('message', handler = event => {
+      this.addEventListener('message', handler = event => {
         try {
           const result = JSON.parse(event.data);
           resolve(result);
         } catch (e) {
           reject(e);
         } finally {
-          this.worker.removeEventListener('message', handler);
+          this.removeEventListener('message', handler);
         }
       });
 
-      this.worker.addEventListener('messageerror', event => {
+      this.addEventListener('messageerror', event => {
         reject(event.data);
       });
 
-      this.worker.postMessage(JSON.stringify(args));
+      this.postMessage(JSON.stringify(args));
     });
   }
 }
