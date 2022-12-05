cd128efd1a0be69e29a3ae8caf33db3608b85684
Rémi Scandella <remi.scandella@gmail.com>
2022-12-05 23:51:53 +0100

    Start work on input-slider

---
+++ components/input-slider/index.php
@@ -0,0 +1,75 @@
+<?php
+$theme = isset($_COOKIE['theme']) ? ($_COOKIE['theme'] == 'light' ? 'light' : ($_COOKIE['theme'] == 'dark' ? 'dark' : 'auto')) : 'auto';
+?>
+
+<!doctype html>
+<html lang="en" data-theme="<?=$theme?>">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&lt;input-slider&gt;</title>
+
+    <!-- ▼ Cache-busted files -->
+    <!--<?php versionizeStart(); ?>-->
+
+    <!-- Import map -->
+    <script defer src="/_common/polyfills/adoptedStyleSheets.min.js"></script>
+    <script>window.esmsInitOptions = { polyfillEnable: ['css-modules', 'json-modules'] }</script>
+    <script defer src="/_common/polyfills/es-module-shims.js"></script>
+    
+    <script type="importmap">
+    {
+      "imports": {
+        "input-slider": "/_common/components/input-slider/input-slider.js"
+      }
+    }
+    </script>
+    
+    <link rel="modulepreload" href="/_common/components/input-slider/input-slider.js">
+    
+    <!--<?php versionizeEnd(__DIR__); ?>-->
+
+    <script type="module">
+      import 'input-slider';
+    </script>
+
+    <style>
+      /*<?php themeSheetStart(); ?>*/
+      html[data-theme="light"] {
+        color-scheme: light;
+        --bg-color: #eee;
+      }
+
+      html[data-theme="dark"] {
+        color-scheme: dark;
+        --bg-color: #111;
+      }
+      /*<?php themeSheetEnd(closeComment: true); ?>*/
+
+      html, body {
+        height: 100%;
+      }
+
+      body {
+        background: var(--bg-color);
+        display: grid;
+        position: relative;
+        gap: .5rem;
+        place-items: center;
+        font-family: system-ui;
+        margin: 0;
+      }
+
+      body > * {
+        grid-row: 1;
+      }
+    </style>
+  </head>
+
+  <body>
+    <input-slider orientation="horizontal"></input-slider>
+    <input-slider orientation="vertical"></input-slider>
+  </body>
+
+</html>
---
+++ components/input-slider/input-slider.js
@@ -0,0 +1,156 @@
+/* Use with this import map:
+<script type="importmap">
+{
+  "imports": {
+    "input-slider": "/_common/components/input-slider/input-slider.js"
+  }
+}
+</script>
+*/
+
+
+
+const template = document.createElement('template');
+template.innerHTML = /*html*/`
+  <div part="slider-rail" role="none" aria-hidden="true"></div>
+  <div part="slider-thumb" role="slider" tabindex="0"></div>
+`;
+
+
+
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(/*css*/`
+  :host {
+    display: grid;
+    place-items: center;
+    position: relative;
+    --tap-safe-size: 44px;
+    --block-size: var(--tap-safe-size);
+    --inline-size: calc(5 * var(--block-size));
+    --cursor-width: 14px;
+  }
+
+  :host([orientation="horizontal"]) {
+    height: var(--block-size);
+    width: var(--inline-size);
+  }
+
+  :host([orientation="vertical"]) {
+    width: var(--block-size);
+    height: var(--inline-size);
+  }
+
+  [part="slider-rail"],
+  [part="slider-thumb"] {
+    grid-row: 1;
+    grid-column: 1;
+  }
+
+  [part="slider-rail"] {
+    width: 100%;
+    height: 100%;
+    border: 2px solid currentColor;
+  }
+
+  :host([orientation="horizontal"]) [part="slider-rail"] {
+    height: 0;
+  }
+
+  :host([orientation="vertical"]) [part="slider-rail"] {
+    width: 0;
+  }
+
+  [part="slider-thumb"] {
+    place-self: start;
+    --size: min(var(--block-size), var(--inline-size));
+    --max-translate: calc(var(--inline-size) - var(--cursor-width));
+    width: var(--size);
+    height: var(--size);
+    box-shadow: inset 0 0 0 2px black,
+                0 0 0 2px white;
+    outline-offset: 5px;
+  }
+
+  :host([orientation="horizontal"]) [part="slider-thumb"] {
+    width: var(--cursor-width);
+    transform: translateX(calc(var(--ratio) * var(--max-translate)));
+  }
+
+  :host([orientation="vertical"]) [part="slider-thumb"] {
+    height: var(--cursor-width);
+    transform: translateY(calc(var(--ratio) * var(--max-translate)));
+  }
+`);
+
+
+
+export class InputSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.adoptedStyleSheets = [sheet];
+  }
+
+
+  connectedCallback() {
+    for (const [attr, obj] of InputSlider.ariaAttributesMap) {
+      this.initAttribute(attr, obj);
+    }
+  }
+
+
+  disconnectedCallback() {
+    
+  }
+
+  initAttribute(attr, obj) {
+    if (!this.getAttribute(attr)) {
+      const mappedAriaAttribute = obj;
+      if (mappedAriaAttribute.default != null) this.setAttribute(attr, mappedAriaAttribute.default);
+    }
+  }
+
+
+  static get observedAttributes() { return ['min', 'max', 'value', 'orientation', 'label', 'labelledby']; }
+
+  static get ariaAttributesMap() {
+    return new Map([
+      ['min', { name: 'aria-valuemin', default: 0 }],
+      ['max', { name: 'aria-valuemax', default: 1 }],
+      ['value', { name: 'aria-valuenow', default: 0 }],
+      ['orientation', { name: 'aria-orientation', default: 'horizontal' }],
+      ['label', { name: 'aria-label', default: null }],
+      ['labelledby', { name: 'aria-labelledby', default: null }],
+    ]);
+  }
+  
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    const slider = this.shadowRoot.querySelector('[role="slider"]');
+    const mappedAriaAttribute = InputSlider.ariaAttributesMap.get(attr);
+
+    // Don't use aria-label when aria-labelledby is set
+    if (attr === 'label' && this.getAttribute('labelledby')) return slider.removeAttribute('aria-label');
+
+    // Set the corresponding aria-attribute on the slider
+    const value = newValue ?? mappedAriaAttribute.default;
+    if (mappedAriaAttribute) slider.setAttribute(mappedAriaAttribute.name, value);
+
+    switch (attr) {
+      case 'value': {
+        const valueTextFormat = this.getAttribute('value-text-format');
+        if (valueTextFormat) {
+          const valueText = eval(valueTextFormat.replace('{v}', value));
+          slider.setAttribute('aria-valuetext', valueText);
+        }
+
+        const min = this.getAttribute('min'), max = this.getAttribute('max');
+        const ratio = 1 - (max - value) / (max - min);
+        this.style.setProperty('--ratio', ratio);
+      } break;
+    }
+  }
+}
+
+if (!customElements.get('input-slider')) customElements.define('input-slider', InputSlider);
