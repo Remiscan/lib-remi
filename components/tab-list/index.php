aa5bad36bf43e2833ec000d364f98343a5226821
Rémi Scandella <remi.scandella@gmail.com>
2022-12-17 18:00:53 +0100

    Add tab-list, more accessible than tab-label

---
+++ components/tab-list/index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>&lt;tab-list&gt;</title>
+
+    <!-- ▼ Cache-busted files -->
+    <!--<?php versionizeStart(); ?>-->
+  
+    <script defer src="../../polyfills/adoptedStyleSheets.min.js"></script> <!-- to support constructed style sheets -->
+    <script>window.esmsInitOptions = { "polyfillEnable": ["css-modules", "json-modules"] }</script>
+    <script defer src="../../polyfills/es-module-shims.js"></script> <!-- to support import maps -->
+    <script defer src="/_common/polyfills/custom-elements.js"></script> <!-- to support "is" attribute -->
+    
+    <script type="importmap">
+    {
+      "imports": {
+        "tab-list": "/_common/components/tab-list/tab-list.js"
+      }
+    }
+    </script>
+  
+    <link rel="modulepreload" href="/_common/components/tab-list/tab-list.js">
+  
+    <!--<?php versionizeEnd(__DIR__); ?>-->
+
+    <script type="module">
+      import 'tab-list';
+    </script>
+
+    <style>
+      /*<?php themeSheetStart(); ?>*/
+      html[data-theme="light"] {
+        color-scheme: light;
+        --bg-color: white;
+        --text-color: black;
+      }
+
+      html[data-theme="dark"] {
+        color-scheme: dark;
+        --bg-color: rgb(34, 34, 34);
+        --text-color: white;
+      }
+
+      :root[data-theme="light"] [role="tablist"] {
+        --hue: 231;
+        --on-bg-color: hsl(var(--hue), 40%, 50%);
+        --hover-bg-color: hsl(var(--hue), 40%, 50%, .3);
+        --active-bg-color: hsl(var(--hue), 40%, 50%, .4);
+        --off-text-color: black;
+        --on-text-color: white;
+      }
+
+      :root[data-theme="dark"] [role="tablist"] {
+        --hue: 217;
+        --on-bg-color: hsl(var(--hue), 89%, 75%);
+        --hover-bg-color: hsl(var(--hue), 89%, 75%, .3);
+        --active-bg-color: hsl(var(--hue), 89%, 75%, .4);
+        --off-text-color: white;
+        --on-text-color: rgb(48, 48, 48);
+      }
+      /*<?php themeSheetEnd(closeComment: true); ?>*/
+
+      html {
+        background: var(--bg-color);
+        height: 100%;
+        width: 100%;
+      }
+
+      body {
+        background: var(--bg-color);
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+        color: var(--text-color);
+        padding: 16px;
+      }
+
+      hr {
+        width: 100%;
+        height: 20px;
+        border: 0;
+      }
+
+      p {
+        margin: 0;
+      }
+
+      /* Prevent flash on unstyled content */
+      [is="tab-list"]:not([role="tablist"]) {
+        display: none;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div is="tab-list" group="tabs-group-name">
+      <button role="tab" aria-controls="controlled-element-11-id">Tab 1.1</button>
+      <button role="tab" aria-controls="controlled-element-12-id">Tab 1.2</button>
+      <button role="tab" aria-controls="controlled-element-13-id">Tab 1.3</button>
+    </div>
+
+    <div id="controlled-element-11-id" hidden>
+      <p>Testing horizontal tab group. First tab automatically selected by default.
+    </div>
+    <div id="controlled-element-12-id" hidden>Content 1.2</div>
+    <div id="controlled-element-13-id" hidden>Content 1.3</div>
+    
+    <hr>
+
+    <div is="tab-list" group="tabs-group-2-name" aria-orientation="vertical" style="--hue: 100">
+      <button role="tab" aria-controls="controlled-element-21-id">Tab 2.1</button>
+      <button role="tab" aria-controls="controlled-element-22-id" aria-selected="true">Tab 2.2</button>
+      <button role="tab" aria-controls="controlled-element-23-id">Tab 2.3</button>
+    </div>
+
+    <div id="controlled-element-21-id" hidden>Content 2.1</div>
+    <div id="controlled-element-22-id" hidden>
+      <p>Testing vertical tab group. Second tab selected by default.
+    </div>
+    <div id="controlled-element-23-id" hidden>Content 2.3</div>
+
+    <hr>
+
+    <div is="tab-list" group="tabs-group-name" style="--hue: 300">
+      <button role="tab" aria-controls="controlled-element-31-id">Tab 3.1</button>
+      <button role="tab" aria-controls="controlled-element-32-id">Tab 3.2</button>
+      <button role="tab" aria-controls="controlled-element-33-id" aria-selected="true">Tab 3.3</button>
+    </div>
+
+    <div id="controlled-element-31-id" hidden>Content 3.1</div>
+    <div id="controlled-element-32-id" hidden>Content 3.2</div>
+    <div id="controlled-element-33-id" hidden>
+      <p>Testing horizontal tab group again. Third tab selected by default.
+    </div>
+  </body>
+
+</html>
---
+++ components/tab-list/tab-list.js
@@ -0,0 +1,294 @@
+/**************************************
+***** EXAMPLE OF USE ******************
+***************************************
+
+<div is="tab-list" group="tabs-group-name">
+  <button role="tab" aria-controls="controlled-element-1-id">Tab 1 name</button>
+  <button role="tab" aria-controls="controlled-element-2-id">Tab 2 name</button>
+  <button role="tab" aria-controls="controlled-element-3-id">Tab 3 name</button>
+</div>
+
+<div id="controlled-element-1-id" hidden></div>
+<div id="controlled-element-2-id" hidden></div>
+<div id="controlled-element-3-id" hidden></div>
+
+**************************************/
+
+
+
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(/*css*/`
+  @layer tab-list {
+
+    [role="tablist"] {
+      display: flex;
+      margin: 0;
+      border: none;
+      gap: 10px;
+      margin: 0 0 10px 0;
+      padding: 0 10px;
+      box-shadow: inset 0 -1px 0 var(--on-bg-color);
+
+      --on-bg-color: hsl(231, 40%, 50%);
+      --hover-bg-color: hsl(231, 40%, 50%, .3);
+      --active-bg-color: hsl(231, 40%, 50%, .4);
+      --off-text-color: black;
+      --on-text-color: white;
+    }
+
+    [role="tablist"][aria-orientation="vertical"] {
+      flex-direction: column;
+      width: fit-content;
+      margin: 0 10px 0 0;
+      padding: 10px 0;
+      box-shadow: inset -1px 0 0 var(--on-bg-color);
+      float: left;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      [role="tablist"] {
+        --on-bg-color: hsl(217, 89%, 75%);
+        --hover-bg-color: hsl(217, 89%, 75%, .3);
+        --active-bg-color: hsl(217, 89%, 75%, .4);
+        --off-text-color: white;
+        --on-text-color: rgb(48, 48, 48);
+      }
+    }
+
+    [role="tab"] {
+      -webkit-appearance: none;
+      appearance: none;
+      background: none;
+      border: none;
+      font: inherit;
+      margin: 0;
+
+      padding: 5px 10px;
+      border: 1px solid var(--on-bg-color);
+      color: var(--off-text-color);
+      border-radius: 5px 5px 0 0;
+      outline-offset: 3px;
+    }
+
+    [role="tablist"][aria-orientation="vertical"] > [role="tab"] {
+      border-radius: 5px 0 0 5px;
+    }
+
+    [role="tab"]:hover {
+      background: var(--hover-bg-color)
+    }
+
+    [role="tab"]:active {
+      background: var(--active-bg-color)
+    }
+
+    [role="tab"][aria-selected="true"] {
+      background: var(--on-bg-color);
+      color: var(--on-text-color);
+    }
+
+    [hidden] {
+      display: none !important;
+    }
+
+  }
+`);
+
+
+
+class TabList extends HTMLDivElement {
+  constructor() {
+    super();
+    this.ready = false;
+    this.initialized = false;
+
+    this.clickHandler = clickEvent => {
+      const clickedTab = clickEvent.currentTarget;
+      this.selectTab(clickedTab);
+    };
+
+    // Handles focus events
+    this.focusHandler = focusEvent => {
+      const tabs = this.allTabs;
+      const selectedTab = focusEvent.currentTarget;
+      const selectedIndex = tabs.findIndex(t => t === selectedTab);
+      const maxIndex = tabs.length;
+
+      const orientation = this.getAttribute('aria-orientation') === 'vertical' ? 'vertical' : 'horizontal';
+
+      const keydownHandler = keydownEvent => {
+        let supportedKey = true;
+        let requestedIndex = null;
+
+        switch (keydownEvent.code) {
+          case 'ArrowLeft': {
+            if (orientation === 'vertical') supportedKey = false;
+            else {
+              requestedIndex = selectedIndex - 1;
+            }
+          } break;
+
+          case 'ArrowRight': {
+            if (orientation === 'vertical') supportedKey = false;
+            else {
+              requestedIndex = selectedIndex + 1;
+            }
+          } break;
+
+          case 'ArrowUp': {
+            if (orientation === 'horizontal') supportedKey = false;
+            else {
+              requestedIndex = selectedIndex - 1;
+            }
+          } break;
+
+          case 'ArrowDown': {
+            if (orientation === 'horizontal') supportedKey = false;
+            else {
+              requestedIndex = selectedIndex + 1;
+            }
+          } break;
+
+          case 'Home': {
+            requestedIndex = 0;
+          } break;
+
+          case 'End': {
+            requestedIndex = -1;
+          } break;
+
+          default: {
+            supportedKey = false;
+          }
+        }
+
+        if (supportedKey) {
+          const requestedTab = tabs?.at(requestedIndex % maxIndex);
+          this.selectTab(requestedTab);
+
+          keydownEvent.preventDefault();
+          keydownEvent.stopPropagation();
+        }
+      };
+
+      const blurHandler = blurEvent => {
+        selectedTab.removeEventListener('keydown', keydownHandler);
+        selectedTab.removeEventListener('blur', blurHandler);
+      };
+
+      selectedTab.addEventListener('keydown', keydownHandler);
+      selectedTab.addEventListener('blur', blurHandler);
+    };
+  }
+
+
+  selectTab(requestedTab = this.selectedTab ?? this.allTabs[0], focus = true) {
+    const tabs = this.allTabs;
+    for (const tab of tabs) {
+      const controlledElement = this.getControlledElement(tab);
+      if (requestedTab === tab) {
+        controlledElement.removeAttribute('hidden');
+        controlledElement.setAttribute('tabindex', '0');
+        tab.setAttribute('aria-selected', 'true');
+        tab.setAttribute('tabindex', '0');
+        if (focus) tab.focus();
+        window.dispatchEvent(new CustomEvent('tabchange', { detail: { group: this.group, value: tab.value } }));
+      } else {
+        controlledElement.setAttribute('hidden', 'hidden');
+        controlledElement.setAttribute('tabindex', '-1');
+        tab.setAttribute('aria-selected', 'false');
+        tab.setAttribute('tabindex', '-1');
+      }
+    }
+  }
+
+
+  get group() {
+    return this.getAttribute('group');
+  }
+
+
+  get allTabs() {
+    return [...this.querySelectorAll(`[role="tab"]`)];
+  }
+
+
+  getControlledElement(tab) {
+    return document.getElementById(tab.getAttribute('aria-controls'));
+  }
+
+
+  get selectedTab() {
+    return this.querySelector('[role="tab"][aria-selected="true"]');
+  }
+
+
+  initializeTabs() {
+    if (this.initialized) return;
+
+    const tabs = this.allTabs;
+    const group = this.group;
+    const orientation = this.getAttribute('aria-orientation') === 'vertical' ? 'vertical' : 'horizontal';
+
+    this.setAttribute('role', 'tablist');
+    this.setAttribute('aria-orientation', orientation);
+
+    tabs.forEach((tab, k) => {
+      const controlledElement = this.getControlledElement(tab);
+      const id = tab.getAttribute('id') ?? `${group}-tab-${k}`;
+      const selected = tab.getAttribute('aria-selected') === 'true';
+
+      // Pass the correct attributes to the tab button
+      tab.setAttribute('type', 'button');
+      tab.setAttribute('id', id);
+      tab.setAttribute('aria-selected', selected ? 'true' : 'false');
+      tab.setAttribute('tabindex', '-1');
+
+      // Pass the correct attributes to the controlled element
+      controlledElement.setAttribute('role', 'tabpanel');
+      controlledElement.setAttribute('aria-labelledby', id);
+      controlledElement.setAttribute('tabindex', '-1');
+      controlledElement.setAttribute('hidden', '');
+    });
+
+    this.initialized = true;
+  }
+
+
+  connectedCallback() {
+    // Add HTML and CSS to the element
+    if (!document.adoptedStyleSheets.includes(sheet))
+      document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+
+    this.initializeTabs();
+
+    const tabs = this.allTabs;
+    tabs.forEach(tab => {
+      tab.addEventListener('click', this.clickHandler);
+      tab.addEventListener('focus', this.focusHandler);
+    });
+
+    this.selectTab(undefined, false);
+  }
+
+
+  disconnectedCallback() {
+    const tabs = this.allTabs;
+    tabs.forEach(tab => {
+      tab.removeEventListener('click', this.clickHandler);
+      tab.removeEventListener('focus', this.focusHandler);
+    });
+  }
+
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue == newValue) return;
+  }
+
+
+  static get observedAttributes() {
+    return [];
+  }
+}
+
+if (!customElements.get('tab-list')) customElements.define('tab-list', TabList, { extends: 'div' });
