ce5f21ac53f582f382bda55574d7582ca3c74ab4
Rémi Scandella <remi.scandella@gmail.com>
2022-12-18 01:24:01 +0100

    Update problem description

--- components/input-switch/input-switch.pointer-events.js
+++ components/input-switch/input-switch.pointer-events.js
@@ -1,6 +1,304 @@
-// Try again when Pointer events, and in particular setCapture, work the same in all browsers
-import sheet from './styles.css' assert { type: 'css' };
-import template from './template.js';
+/***** CURRENT PROBLEM WITH POINTER EVENTS: *****
+
+On pointerdown, setPointerCapture is used to make sure every following event is fired on the button, even if the pointer moved.
+However:
+- In Firefox, after a pointerup event, a click event is fired on the button ONLY IF the pointer is still over the button.
+- In Chrome, after a pointerup event, a click event is fired on the button no matter where the pointer is.
+
+Chrome's behaviour is the one I want here.
+
+(see https://github.com/w3c/pointerevents/issues/356)
+
+*************************************************/
+
+
+
+const template = document.createElement('template');
+template.innerHTML = /*html*/`
+  <button type="button" role="switch" aria-checked="false" part="button">
+    <span class="input-switch-bg" aria-hidden="true" part="background">
+      <span class="input-switch-hints" aria-hidden="true" part="hints-container">
+        <span data-state="on" part="hint hint-on"></span>
+        <svg class="input-switch-handle" viewBox="0 0 24 24" part="handle-container">
+          <circle cx="12" cy="12" r="9.6" stroke-width="0" part="handle"/>
+          <circle class="focus-dot" cx="12" cy="12" r="2.4" stroke-width="0" part="focus-dot"/>
+        </svg>
+        <span data-state="off" part="hint hint-off"></span>
+      </span>
+    </span>
+  </button>
+`;
+
+
+
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(/*css*/`
+  :host {
+    aspect-ratio: 1 / 2;
+    --width: 4rem;
+    width: var(--width);
+    height: calc(.5 * var(--width));
+
+    display: inline-block;
+    position: relative;
+
+    --easing-standard: cubic-bezier(0.4, 0.0, 0.2, 1);
+    --easing-decelerate: cubic-bezier(0.0, 0.0, 0.2, 1);
+    --easing: var(--easing-standard);
+    --duration: .2s;
+    --font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+
+    --off-bg-color: #828282; /* --on-bg-color to OKLCH, then chroma 0 and lightness +10% */
+    --on-bg-color: hsl(231, 40%, 50%);
+    --handle-color: #ebebeb;
+    --handle-hover-color: #ddd;
+    --handle-active-color: #fff;
+    --off-text-color: var(--handle-color);
+    --on-text-color: var(--handle-color);
+    --focus-color: black;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :host {
+      --off-bg-color: #929292; /* --on-bg-color to OKLCH, then chroma 0 and lightness -10% */
+      --on-bg-color: hsl(217, 89%, 75%);
+      --handle-color: rgb(48, 48, 48);
+      --handle-hover-color: #3a3a3a;
+      --handle-active-color: #222;
+      --off-text-color: var(--handle-color);
+      --on-text-color: var(--handle-color);
+      --focus-color: white;
+    }
+  }
+
+  @media (forced-colors: active) {
+    :host {
+      -ms-high-contrast-adjust: none;
+      forced-color-adjust: none;
+      --off-bg-color: ButtonFace;
+      --on-bg-color: Highlight;
+      --handle-color: ButtonText;
+      --handle-hover-color: Highlight;
+      --handle-active-color: Highlight;
+      --off-text-color: ButtonText;
+      --on-text-color: Canvas;
+      --focus-color: Highlight;
+    }
+  }
+
+  [role="switch"] {
+    border: none;
+    margin: 0;
+    padding: 0;
+    -webkit-appearance: none;
+    appearance: none;
+    font: inherit;
+    color: inherit;
+    outline-offset: 3px;
+
+    width: 100%;
+    height: 100%;
+    border-radius: calc(.5 * var(--width));
+
+    touch-action: none;
+    --ratio: 0;
+    --dir: 1;
+    --transform-ratio: calc(var(--dir) * var(--ratio));
+  }
+
+  [role="switch"]:dir(rtl) {
+    --dir: -1;
+  }
+
+  .input-switch-bg {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 100%;
+    width: 100%;
+    height: 100%;
+    border-radius: calc(.5 * var(--width));
+    background-color: var(--off-bg-color);
+    position: relative;
+    overflow: hidden;
+  }
+
+  .input-switch-bg::before {
+    content: '';
+    display: block;
+    grid-row: 1 / -1;
+    grid-column: 1 / -1;
+    width: 100%;
+    height: 100%;
+    background-color: var(--on-bg-color);
+    opacity: calc(1 - var(--ratio));
+    transition: opacity var(--duration) var(--easing);
+    will-change: opacity;
+    z-index: 1;
+  }
+
+  @media (forced-colors: active) {
+    [role="switch"] {
+      border: 2px solid var(--handle-color);
+      background-color: var(--off-bg-color);
+    }
+
+    [role="switch"][aria-checked="true"] {
+      border-color: var(--on-bg-color);
+      background-color: var(--on-bg-color);
+    }
+
+    [role="switch"][aria-checked="true"] .input-switch-bg {
+      background-color: var(--on-bg-color);
+    }
+
+    [role="switch"][aria-checked="true"] .input-switch-handle {
+      fill: var(--on-text-color);
+    }
+
+    [role="switch"]:not(:disabled):is(:hover, :active, .active) {
+      background-color: var(--off-bg-color);
+      border-color: var(--on-bg-color);
+      --on-text-color: var(--off-text-color);
+    }
+
+    [role="switch"]:not(:disabled):is(:hover, :active, .active) .input-switch-bg {
+      background-color: var(--off-bg-color);
+    }
+
+    [role="switch"]:not(:disabled):is(:hover, :active, .active) .input-switch-handle {
+      fill: var(--off-text-color);
+    }
+
+    .input-switch-bg::before {
+      display: none;
+    }
+
+    .focus-dot {
+      display: none;
+      fill: var(--off-bg-color);
+    }
+    [role="switch"][aria-checked="true"] .focus-dot {
+      fill: var(--on-bg-color);
+    }
+    [role="switch"]:not(:disabled):is(:hover, :active, .active) .focus-dot {
+      fill: var(--off-bg-color);
+    }
+    [role="switch"]:focus .focus-dot {
+      display: block;
+    }
+    [role="switch"]:focus:not(:focus-visible) .focus-dot {
+      display: none;
+    }
+  }
+
+  .input-switch-hints {
+    grid-row: 1 / -1;
+    grid-column: 1 / -1;
+    width: 150%;
+    height: 100%;
+    display: grid;
+    place-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 100%;
+    place-items: center;
+    transition: transform var(--duration) var(--easing);
+    z-index: 2;
+    transform: translateX(calc(-100% * var(--transform-ratio) / 3));
+    will-change: transform;
+  }
+
+  .input-switch-hints>span[data-state] {
+    display: grid;
+    --side-margin: calc(.1 * var(--width) / 3);
+    font-family: var(--font-family);
+    font-size: calc(var(--width) / 4);
+    font-weight: var(--font-weight, 700);
+    max-width: 100%;
+    max-height: 100%;
+    overflow: hidden;
+  }
+
+  .input-switch-hints>span[data-state]>svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  .input-switch-hints>span[data-state="on"] {
+    place-items: start;
+  }
+  .input-switch-hints>span[data-state="off"] {
+    place-items: end;
+  }
+
+  .input-switch-hints>span>* {
+    --padding-left: 0;
+    --padding-right: 0;
+    padding: 0 var(--padding-right) 0 var(--padding-left);
+  }
+
+  .input-switch-hints>span[data-state="on"]>* {
+    --padding-left: calc((1 + var(--dir)) * .5 * var(--side-margin));
+    --padding-right: calc((1 - var(--dir)) * .5 * var(--side-margin));
+  }
+  .input-switch-hints>span[data-state="off"]>* {
+    --padding-left: calc((1 - var(--dir)) * .5 * var(--side-margin));
+    --padding-right: calc((1 + var(--dir)) * .5 * var(--side-margin));
+  }
+
+  .input-switch-hints svg.default-icon {
+    stroke-width: var(--stroke-width, 3);
+  }
+
+  .input-switch-hints>[data-state="on"] {
+    grid-row: 1;
+    grid-column: 1;
+    color: var(--on-text-color);
+    stroke: var(--on-text-color);
+  }
+
+  .input-switch-hints>[data-state="off"] {
+    grid-row: 1;
+    grid-column: 3;
+    color: var(--off-text-color);
+    stroke: var(--off-text-color);
+  }
+
+  .input-switch-handle {
+    grid-row: 1;
+    grid-column: 2;
+    width: 100%;
+    height: 100%;
+    z-index: 3;
+    fill: var(--handle-color);
+  }
+
+  [role="switch"]:not(:disabled):hover .input-switch-handle {
+    fill: var(--handle-hover-color);
+  }
+  [role="switch"]:not(:disabled):is(:active, .active) .input-switch-handle {
+    fill: var(--handle-active-color);
+  }
+
+  [role="switch"][aria-checked="false"] {
+    --ratio: 1;
+    --current-color: var(--off-bg-color);
+  }
+  [role="switch"][aria-checked="true"] {
+    --ratio: 0;
+    --current-color: var(--on-bg-color);
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    :host {
+      --duration: 0;
+    }
+  }
+`);
+
+
+
+// How many pixels the finger must move to consider it an intentional drag
+const intentionalDragLimit = 3; // pixels
 
 
 
@@ -28,45 +326,12 @@ export default class InputSwitch extends HTMLElement {
 
 
   // Clicking on the label toggles the switch
-  onLabelDown(event) {
-    const label = event.currentTarget;
-
-    if (event.composedPath().includes(this.button)) return;
-    label.setPointerCapture(event.pointerId);
-    this.moving = false;
-    this.button.style.removeProperty('--duration');
-    this.button.style.removeProperty('--easing');
-
-    // Swiping on the label (or selecting its text) shouldn't toggle the switch
-    let cancel = false;
-    const iniX = event.clientX;
-
-    const labelMove = event => {
-      if (!cancel & Math.abs(iniX - event.clientX) > 5) cancel = true;
+  onLabelClick(event) {
+    if (event.composedPath().includes(this.button)) {
+      event.preventDefault();
+    } else {
+      this.button.click();
     }
-
-    const labelStop = event => {
-      if (event.target != label) cancel = true;
-    };
-    
-    const labelUp = event => {
-      if (event.type === 'touchend') {
-        event.preventDefault(); // Prevents mouseup event which would fire labelUp again
-      }
-      if (!(event.composedPath().includes(this.button) || this.moving || cancel)) {
-        this.button.focus();
-        this.toggle(); // Toggle instead of click, to work after a small move that canceled the previous click
-      }
-
-      label.releasePointerCapture(event.pointerId);
-      label.removeEventListener('touchstart', labelStop, { passive: true });
-      label.removeEventListener('pointermove', labelMove, { passive: true });
-      label.removeEventListener('pointerup', labelUp, { passive: false });
-    };
-
-    label.addEventListener('touchstart', labelStop, { passive: true });
-    label.addEventListener('pointermove', labelMove, { passive: true });
-    label.addEventListener('pointerup', labelUp, { passive: false });
   }
 
 
@@ -85,7 +350,7 @@ export default class InputSwitch extends HTMLElement {
     this.button.style.removeProperty('--easing');
 
     const coords = this.getBoundingClientRect();
-    const width = 0.5 * coords.width * (1 - .2 * .5);
+    const slidableWidth = 0.5 * coords.width * (1 - .2 * .5);
     const textDir = this.rtl ? -1 : 1;
     this.button.style.setProperty('--dir', textDir); // for broswers that don't support the :dir() pseudo-class
     
@@ -94,14 +359,16 @@ export default class InputSwitch extends HTMLElement {
     const initialTouch = getCoords(event);
 
     // Ratio of (distance moved) / (button width)
-    const updateDistance = touch => textDir * Math.max(-1, Math.min((touch.x - initialTouch.x) / width, 1));
-    const updateRatio = touch => Math.max(0, Math.min(initialRatio - updateDistance(touch), 1));
     const initialRatio = Number(this.button.getAttribute('aria-checked') != 'true');
+    const updateDistance = touch => textDir * Math.max(-1, Math.min((touch.x - initialTouch.x) / slidableWidth, 1));
+    const updateRatio = touch => Math.max(0, Math.min(initialRatio - updateDistance(touch), 1));
 
     let lastTouch = initialTouch;
     let lastDistance = 0;
     let maxDistance = 0;
     let frameReady = true;
+
+    const clickSafetyMargin = intentionalDragLimit / slidableWidth; // % of slidable width
 
     const moveHandle = event => {
       if (!frameReady) return;
@@ -118,13 +385,13 @@ export default class InputSwitch extends HTMLElement {
       const distance = updateDistance(lastTouch);
 
       // Restart timer when direction changes, to save only the inertia of the last move
-      if (Math.sign(distance) != Math.sign(lastDistance)) time = Date.now();
+      if (Math.sign(distance) !== Math.sign(lastDistance)) time = Date.now();
 
       lastDistance = distance;
       maxDistance = Math.max(Math.abs(distance), maxDistance);
 
       // Safety margin to differentiate a click and a drag
-      if (!this.moving && Math.abs(distance) > 0.1) this.moving = true;
+      if (!this.moving && Math.abs(distance) > clickSafetyMargin) this.moving = true;
       this.button.style.setProperty('--ratio', ratio);
 
       requestAnimationFrame(() => { frameReady = true });
@@ -144,19 +411,16 @@ export default class InputSwitch extends HTMLElement {
       } else {
         this.button.style.removeProperty('--duration');
         // If it's not a click (over safety margin)
-        if (maxDistance > 0.1) this.cancelNextClick = true;
+        if (maxDistance > clickSafetyMargin) this.cancelNextClick = true;
       }
 
-      this.button.releasePointerCapture(event.pointerId);
+      //this.button.releasePointerCapture(event.pointerId);
       this.button.removeEventListener('pointermove', moveHandle, { passive: true });
       this.button.removeEventListener('pointerup', endHandle, { passive: true });
     };
 
     this.button.addEventListener('pointermove', moveHandle, { passive: true });
     this.button.addEventListener('pointerup', endHandle, { passive: true });
-    for (const type of ['pointerover', 'pointerenter', 'pointercancel', 'pointerout', 'pointerleave', 'gotpointercapture', 'lostpointercapture', 'pointerdown', 'pointermove', 'pointerup', 'click']) {
-      this.button.addEventListener(type, event => console.log(event.type, Date.now()));
-    }
   }
 
 
@@ -242,8 +506,8 @@ export default class InputSwitch extends HTMLElement {
       }
 
       // Clicking on the label toggles the switch
-      this.handlers.labelDown = this.onLabelDown.bind(this);
-      label.addEventListener('pointerdown', this.handlers.labelDown, { passive: true });
+      this.handlers.labelClick = this.onLabelClick.bind(this);
+      label.addEventListener('click', this.handlers.labelClick);
     }
 
     // Make switch touchmoveable
@@ -264,9 +528,13 @@ export default class InputSwitch extends HTMLElement {
         this.toggle();
       }
 
-      if (!event.pointerId) this.button.focus();
+      //if (!event.pointerId) this.button.focus();
     }
     this.button.addEventListener('click', clickHandle);
+
+    for (const type of ['pointercancel', 'gotpointercapture', 'lostpointercapture', 'pointerdown', 'pointerup', 'click']) {
+      this.button.addEventListener(type, event => console.log(event.type, event.currentTarget, Date.now()));
+    }
 
     this.button.style.removeProperty('--duration');
   }
@@ -275,7 +543,7 @@ export default class InputSwitch extends HTMLElement {
   disconnectedCallback() {
     const id = this.getAttribute('id');
     const label = document.querySelector(`label[for="${id}"]`);
-    if (label) label.removeEventListener('pointerdown', this.handlers.labelDown, { passive: true });
+    if (label) label.removeEventListener('click', this.handlers.labelClick);
     this.button.removeEventListener('pointerdown', this.handlers.start, { passive: true });
   }
 
