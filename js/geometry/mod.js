d6200265063133611ffae3f69849079f0f393016
Rémi Scandella <remi.scandella@gmail.com>
2024-12-01 09:56:06 +0000

    Add return types

--- js/geometry/mod.js
+++ js/geometry/mod.js
@@ -8,23 +8,26 @@ export class Point2D {
     this.y = y;
   }
 
+  /** @returns {this} */
   translate(dx = 0, dy = 0) {
-    return new Point2D(
+    return new this.constructor(
       this.x + dx,
       this.y + dy
     );
   }
 
+  /** @returns {this} */
   rotate(angle = 0) {
     const rad = angle * Math.PI / 180;
-    return new Point2D(
+    return new this.constructor(
       Math.cos(rad) * this.x - Math.sin(rad) * this.y,
       Math.sin(rad) * this.x + Math.cos(rad) * this.y
     );
   }
 
+  /** @returns {this} */
   scale(sx = 1, sy = sx) {
-    return new Point2D(
+    return new this.constructor(
       sx * this.x,
       sy * this.y
     );
@@ -44,51 +47,60 @@ export class Point3D {
     this.z = z;
   }
 
+  /** @returns {this} */
   translate(dx = 0, dy = 0, dz = 0) {
-    return new Point3D(
+    return new this.constructor(
       this.x + dx,
       this.y + dy,
       this.z + dz
     );
   }
 
+  /** @returns {this} */
   scale(sx = 1, sy = sx, sz = sx) {
-    return new Point3D(
+    return new this.constructor(
       sx * this.x,
       sy * this.y,
       sz * this.z
     );
   }
 
-  /** Rotate around axis of vector [vx, vy, vz]. */
+  /**
+   * Rotate around axis of vector [vx, vy, vz].
+   * @returns {this}
+   */
   rotate(vx = 0, vy = 0, vz = 0, angle = 0) {
     const rad = angle * Math.PI / 180;
     const sc = Math.sin(rad / 2) * Math.cos(rad / 2);
     const sq = Math.sin(rad / 2) ** 2;
     const d = Math.sqrt(vx ** 2 + vy ** 2 + vz ** 2);
     const [rx, ry, rz] = [vx, vy, vz].map(v => v / d);
-    return new Point3D(
+    return new this.constructor(
       (1 - 2 * (ry ** 2 + rz ** 2) * sq) * this.x + (2 * (rx * ry * sq - rz * sc)) * this.y + (2 * (rx * rz * sq + ry * sc)) * this.z,
       (2 * (rx * ry * sq + rz * sc)) * this.x + (1 - 2 * (rx ** 2 + rz ** 2) * sq) * this.y + (2 * (ry * rz * sq - rx * sc)) * this.z,
       (2 * (rx * rz * sq - ry * sc)) * this.x + (2 * (ry * rz * sq + rx * sc)) * this.y + (1 - 2 * (rx ** 2 + ry ** 2) * sq) * this.z
     );
   }
 
+  /** @returns {this} */
   rotateX(angle = 0) {
     return this.rotate(1, 0, 0, angle);
   }
 
+  /** @returns {this} */
   rotateY(angle = 0) {
     return this.rotate(0, 1, 0, angle);
   }
 
+  /** @returns {this} */
   rotateZ(angle = 0) {
     return this.rotate(0, 0, 1, angle);
   }
 
+  /** @returns {this} */
   perspective(d = Infinity) {
     const w = 1 - this.z / d;
-    return new Point3D(
+    return new this.constructor(
       (1 / w) * this.x,
       (1 / w) * this.y,
       (1 / w) * this.z
